ba0254626236705d4be8f6c5f25925aa738b408c
Ausar McGruder <ausarmcgruder@Ausars-MacBook-Pro.local>
2019-12-17 14:56:19 -0500

    #12..snapshots for NewMovie component

--- src/NewMovie.test.js
+++ src/NewMovie.test.js
@@ -6,12 +6,14 @@ import NewMovie from './NewMovie';
 afterEach(cleanup);
 
 test('<NewMovie>', () => {
-    const { debug, getByTestId, queryByTestId } = render(<NewMovie />);
-    debug();
+    const { debug, getByTestId, queryByTestId, container } = render(<NewMovie />);
+    // debug();
 
     //getByTestId renders the component then grabs it by the "page-title" data-testid
     expect(getByTestId("page-title").textContent).toBe('New Movie')
     
     //searching to see if that id exists
     expect(queryByTestId('movie-form')).toBeTruthy();
+
+    expect(container.firstChild).toMatchSnapshot();
 });
