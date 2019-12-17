7af45be8dafad0284f4bf3977fdae6b4fec37282
Ausar McGruder <ausarmcgruder@Ausars-MacBook-Pro.local>
2019-12-17 12:18:26 -0500

    added count functionality and tests for count

--- src/Counter.js
+++ src/Counter.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react'
 export default class Counter extends Component {
     state = {
         count: 0,
-
+    }
+    count = () => {
+        this.setState(prevState => ({ 
+            count: prevState.count + 1 ,
+        }))
     }
     render() {
         const { count } = this.state;
         return (
             <div className="Hello">
-                <button data-testid="counter-button">{ count }</button>
+                <button onClick={this.count} data-testid="counter-button">{ count }</button>
             </div>
         )
     }
--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import Counter from './Counter';
 
 //wrapper.debug() to show what the dom is rendering
 test('<Counter />', () =>{
     // alternate render method - const wrapper = render(<Counter />);
     const {debug, getByTestId} = render(<Counter />);
+    const counterButton = getByTestId('counter-button');
     // debug();
 
     // asserts counter-button is a button
-    expect(getByTestId('counter-button').tagName).toBe('BUTTON')
+    expect((counterButton).tagName).toBe('BUTTON')
 
     // asserts counter-button starts at 0
-    expect(getByTestId('counter-button').textContent).toBe('0')
+    expect((counterButton).textContent).toBe('0')
+
+    fireEvent.click(counterButton);
+    expect(counterButton.textContent).toBe('1');
+
+    fireEvent.click(counterButton);
+    expect(counterButton.textContent).toBe('2')
+
 });
